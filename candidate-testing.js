a2f6959e30a96d6dd515aa013c7517d7e4889bd4
crimsonRIBBON <84585332+crimsonRIBBON@users.noreply.github.com>
2021-06-17 03:06:10 +0000

    I finished itttttttt. Plz treat me kindly github grader.

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,53 +8,57 @@ let candidateName = '';
 let question = ("Who was the first American woman in space? ");
 let correctAnswer = 'Sally Ride';
 let candidateAnswer = '';
-let questions = ["Who was the first American woman in space?","True or False: 5 kilometer == 5000 meters?","(5+3)/2*10=?","Given the array [8,'Orbit','Trajectory',45], what entry s at index 2?","What is the minimum crew size for the ISS"];
+let questions = ["1) Who was the first American woman in space?","2) True or False: 5 kilometer == 5000 meters?","3) (5+3)/2*10=?","4) Given the array [8,'Orbit','Trajectory',45], what entry s at index 2?","5) What is the minimum crew size for the ISS"];
 let correctAnswers = ["Sally Ride","true","40","Trajectory","3"];
 let candidateAnswers = [ ];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
- candidateName = input.question('Enter your first name: ');
- 
+ candidateName = input.question('Candidate Name: ');
+ console.log(`\nCandidate Name: ${candidateName}`);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
  
- ;
- for (i = 0; i < questions.length;i++) {
-candidateAnswers.push(input.question(questions[i]));
-}
  
+ for (let i = 0; i < questions.length;i++) {
+    candidateAnswer = input.question(questions[i]);
+    candidateAnswers.push(candidateAnswer);
+
+    console.log(`Correct answer: ${correctAnswers[i]}\nYour answer: ${candidateAnswers[i]}\n`);
+}
+
+
 } 
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
-  let space = '   '
-console.log(space)
-  let congrats = `You finished the test, Congrats!
-Now let's check your score.
-Below we listed the correct answers on the left, and yours on the right. Tally them up to get your grade.`
+  let numberOfCorrectAnswers = [];
 
-console.log(congrats)
-console.log(space)
-for (i = 0; i < correctAnswers.length; i++) 
- { console.log(correctAnswers[i] + ' ' + candidateAnswers[i]);}
- 
-
-
-/* if (candidateAnswer === correctAnswer) {
-   console.log("That is correct! Great Job!");
- }
-   else {
-     console.log("That answer is not correct. Try again!");
-   }
- 
-*/
-  let grade;
   
+
+  for (let i = 0; i < candidateAnswers.length;i++){
+    let lowerCaseCandidateAnswers = candidateAnswers[i].toLowerCase();
+    let lowerCaseCorrectAnswers = correctAnswers[i].toLowerCase();
+    if (lowerCaseCandidateAnswers === lowerCaseCorrectAnswers){
+    numberOfCorrectAnswers.push(candidateAnswers[i]);}
+  }
+
+
+  let grade = (numberOfCorrectAnswers.length / questions.length * 100);
+
+// make answers case insisitive when checking
+// dont alter origional correct answer
+let stats = [];
+
+if (grade >= 80) { 
+  stats.push('PASSED');
+} else {stats.push('FAILED');}
+  
+console.log(`>>> Overall Grade: ${grade}% (${numberOfCorrectAnswers.length} of ${candidateAnswers.length} responses correct) <<<\n>>> Status: ${stats} <<<`)
 
   return grade;
 }
